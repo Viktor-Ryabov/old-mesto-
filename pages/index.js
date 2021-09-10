9b54b16f38b75f6e04e99e40312f8d706996cc98
Viktor-Ryabov <84191251+Viktor-Ryabov@users.noreply.github.com>
2021-09-10 12:19:22 +0300

    функция добавления карточки

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 //ЭЛЕМЕНТЫ
-//ФОРМА ПРОФАЙЛА
+//фотографии для карточек
+
+
+//кнопка лайк
+const buttonLike = Array.from(document.querySelectorAll('.card__button-like'));
+
+//ПРОФАЙЛ
 const editForm = document.querySelector('#editForm');
 const nameEditForm = document.querySelector('#nameEditForm'); //сокращенная запись
 const descriptionEditForm = document.querySelector('#descriptionEditForm');
@@ -8,15 +14,17 @@ const saveProfile = document.querySelector('#saveData');
 //ЭДЕМЕНТЫ ФОРМЫ ПРОФАЙЛА
 const nameProfile = document.querySelector('#nameProfile');
 const descriptionProfile = document.querySelector('#descriptionProfile');
-
-//ПОПАД ДОБАВИТЬ КАРТОЧКУ
 const editFormProfile = document.querySelector('#editFormProfile');
 const popupOpenProfile = document.querySelector('#popupOpenProfile');
 const popupCloseProfile = document.querySelector('#popupCloseProfile');
 
+//ПОПАП ДОБАВИТЬ КАРТОЧКУ
 const editFormMesto = document.querySelector('#editFormMesto');
 const buttonAddCard = document.querySelector('#buttonAddCard');
 const popapCloseCard = document.querySelector('#popapCloseCard');
+const mestoName = document.querySelector('#mestoName');
+const linkFotoMesto = document.querySelector("#linkFotoMesto");
+
 
 //ПОПАП ФОТО
 const imagePopup = document.querySelector('#image-popup');
@@ -25,9 +33,86 @@ const popapCloseFoto = document.querySelector('#popapCloseFoto');
 const imagePopupFotoBig = document.querySelector('.image-popup__foto');
 const imagePopupFotoText = document.querySelector('.image-popup__discription');
 
+//Удаление карточки
+const deleteCardButton = Array.from(document.querySelectorAll(".card__delete-button"));
+const cardPlace = document.querySelectorAll('.card');
+const initialCards = [
+    {
+      name: 'Ай Петри',
+      link: 'https://disk.yandex.ru/i/VlGyQNDCAwPulw'
+    },
+    {
+      name: 'Сменцево',
+      link: 'https://disk.yandex.ru/i/CZ6nuCYtCmi4eQ'
+    },
+    {
+      name: 'Архыз',
+      link: 'https://disk.yandex.ru/i/nEKKiNigqaqXLQ'
+    },
+    {
+      name: 'Перевал Голубева',
+      link: 'https://disk.yandex.ru/i/lHuQ8fFyikj62A'
+    },
+    {
+      name: 'Карелия',
+      link: 'https://disk.yandex.ru/i/5UoXdA0Qg0UaAw'
+    },
+    {
+      name: 'Домбай',
+      link: 'https://disk.yandex.ru/i/ljiysFENdEeJdw'
+    }
+    ];
+
+    console.log(initialCards[2].name);
+
+// for(let i = 0; i < cardPlace.length; i++){
+//     cardPlace[i].src = initialCards[i].link;
+//     cardPlace[i].alt = initialCards[i].name;
+//     cardPlace[i].innerHTML = initialCards[i].name;
+// }
+
+    
+//Добавление карточки
+const cardPlacesSection = document.querySelector('.places');
+if ('content' in document.createElement('template')) {
+    const newCardTemplate = document.querySelector('#newCardTemplate').content;
+    editFormMesto.addEventListener('submit', function(event){
+        event.preventDefault();
+        let clone = newCardTemplate.cloneNode(true);
+        console.log(clone.querySelector('.card__title'));
+        clone.querySelector('.card__title').innerHTML = mestoName.value;
+        clone.querySelector('img').src = linkFotoMesto.value;
+        clone.querySelector('img').alt = mestoName.value;
+       
+        const deleteCardButtonNew = clone.querySelector('.card__delete-button');
+        deleteCardButton.push(deleteCardButtonNew);
+        console.log(deleteCardButton);
+
+        const buttonLikeNew = clone.querySelector('.card__button-like');
+        buttonLike.push(buttonLikeNew);
+
+        fotoOpen(imagePopupOpenNew);
+
+        
+        cardPlacesSection.append(clone);
+        //слушатели событий
+        
+        
+
+        editFormMesto.style.visibility = 'hidden ';       
+    });  
+}
 
 
-//Функция "больше фото" открыть
+
+//цикл перебора для удаления карточек
+for(let i = 0; i < deleteCardButton.length; i++){
+    deleteCardButton[i].addEventListener('click', function(){
+        cardPlace[i].remove();
+    });
+}
+
+//Функция "большие фото" открыть
 fotoOpen = function(imageClick) {
     for(let i = 0; i < imagePopupOpen.length; i++) {
             imageClick[i].addEventListener('click', function () {
@@ -75,7 +160,6 @@ function rename () {
 rename();
 
 //КНОПКА ЛАЙК
-const buttonLike = document.querySelectorAll('.card__button-like');
 buttonLike.forEach((item) => {
     item.addEventListener('click', function() {
         item.classList.toggle('card__button-like_active');
